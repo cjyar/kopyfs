9b59db76c616b3993950f2426013510eff486a4a
Chris Jones <chris@cjones.org>
2020-05-07 08:50:53 -0600

    checkpoint

--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -1,6 +1,8 @@
-use hyper::body::HttpBody;
 use k8s_openapi::api::core::v1 as corev1;
-use k8s_openapi::{ResponseError, WatchResponse};
+use kube::api::Api;
+use kube::client::Client;
+use kube::config::Config;
+use kube::runtime::Informer;
 use std::error::Error;
 
 extern crate kopyfs;
@@ -8,26 +10,21 @@ extern crate kopyfs;
 /// Monitors PVCs and PVs, and local volumes.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let client = kopyfs::Client::new()?;
+    let k8scfg = Config::infer();
+    let k8s = Client::new(k8scfg);
+    let pvcs: Api<corev1::PersistentVolumeClaim> = Api::all(k8s);
+    let inform = Informer::new(pvcs);
 
-    let (request, response_body) =
-        corev1::PersistentVolumeClaim::watch_persistent_volume_claim_for_all_namespaces(
-            Default::default(),
-        )?;
-
-    let mut response = client.request(request).await?;
-    let status_code = response.status();
-    let mut response_body = response_body(status_code);
-    while let Some(chunk) = response.body_mut().data().await {
-        let chunk = chunk?;
-        response_body.append_slice(&chunk);
-        match response_body.parse() {
-            Ok(WatchResponse::Ok(pvc_list)) => println!("{:?}", pvc_list),
-            Ok(WatchResponse::Other(x)) => println!("Got unexpected type {:?}", x),
-            Err(ResponseError::NeedMoreData) => continue,
-            Err(x) => println!("Error parsing PVC: {:?}", x),
+    let pvcs = inform.poll().await?.boxed();
+    while let Some(event) = pvcs.try_next().await? {
+        match event {
+            WatchEvent::Added(pvc) => println!("{}", pvc),
+            WatchEvent::Modified(pvc) => println!("{}", pvc),
+            WatchEvent::Deleted(pvc) => println!("{}", pvc),
         }
     }
+
+    kopyfs::WrappedError;
 
     Ok(())
 }
